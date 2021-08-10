5fd71832c0caf2166a24951699d72533f5a9a225
Cristina De Rito <artemisia-absynthium@users.noreply.github.com>
2021-08-10 16:49:17 +0200

    Implement light/dark OS theme support + app header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-name">FOODA</h1>
       </header>
+      <main className="Main">
+        <h3 className="centered">
+          Fresh food at your door
+        </h3>
+      </main>
     </div>
   );
 }
