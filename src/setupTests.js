dd8c114d9eebfa25558894ba1643bdfc96ebfb65
Cristina De Rito <artemisia-absynthium@users.noreply.github.com>
2021-09-05 18:13:22 +0200

    Fix test and remove redundant test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,28 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-
-import App from "./App";
-
-let container = null;
-beforeEach(() => {
-    // setup a DOM element as a render target
-    container = document.createElement("div");
-    document.body.appendChild(container);
-});
-
-afterEach(() => {
-    // cleanup on exiting
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-});
-
-it("renders", () => {
-    act(() => {
-        render(<App />, container);
-    });
-    expect(container.textContent).toContain("Learn React");
-});
